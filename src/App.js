20cd1103f1b85981238edde8b8546b25013dc4d9
Denisse Perez Botello <21240007@smartin.tecnm.mx>
2025-11-10 14:49:38 -0600

    Agregue la vista Usuarios.jsx a la carpeta views

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./components/BarraNavegacion";
-import Sidebar from "./components/MenuLateral";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Usuarios from "./views/Usuarios";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Navbar />
-        <div className="app-layout">
-          <Sidebar />
-          <main className="main-content">
-            <h1>Bienvenido al sistema</h1>
-            <p>Selecciona una opción del menú lateral.</p>
-          </main>
-        </div>
-      </div>
-      
+     <Router>
+      <Routes>
+
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Usuarios />} />
+          <Route path="usuarios" element={<Usuarios />} />
+        </Route>
+
+      </Routes>
     </Router>
   );
 }
